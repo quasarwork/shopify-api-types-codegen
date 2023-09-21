3a18eb9be358e5d24ae9b49da0e6b2ada7763be6
aurelienbobenriethpro <aurelien.bobenrieth@gmail.com>
2023-09-21 16:02:20 +0200

    🔥 remove unnecessary build

--- src/codegen/utils/generate-unfetchable-api-aliases-types.ts
+++ src/codegen/utils/generate-unfetchable-api-aliases-types.ts
@@ -24,7 +24,7 @@ export const generateUnfetchableApiAliasesTypes = (
 
     const typesDestinationDir = join(
       __dirname,
-      `../../${outDir}/api/${api.slug}/${apiVersion.name}`,
+      `../../../${outDir}/api/${api.slug}/${apiVersion.name}`,
     );
 
     if (!existsSync(typesDestinationDir))
